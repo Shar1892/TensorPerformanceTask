0fc000dcf22bf5bfdec3689a051b33bee0466e09
sg.sharov <sg.sharov@tensor.ru>
2022-03-10 17:34:31 +0300

    задача по оптимизации

--- src/contacts.js
+++ src/contacts.js
@@ -1,26 +1,37 @@
-const contacts = document.getElementsByClassName("contacts")[0];
-const stickyHeader = document.getElementsByClassName("stickyHeader")[0];
+const contacts = document.getElementsByClassName('contacts')[0];
+const stickyHeader = document.getElementsByClassName('stickyHeader')[0];
 
 function addContacts() {
-  const fragment = document.createDocumentFragment();
-  for (let i = 0; i < 50000; i++) {
-    const child = document.createElement("div");
-    child.textContent = i;
-    child.classList.add("contact");
-    fragment.appendChild(child);
-  }
-  contacts.appendChild(fragment);
+	const fragment = document.createDocumentFragment();
+	for (let i = 0; i < 50000; i++) {
+		const child = document.createElement('div');
+		child.textContent = i;
+		child.classList.add('contact');
+		fragment.appendChild(child);
+	}
+	contacts.appendChild(fragment);
 }
 
-contacts.addEventListener("scroll", (e) => {
-  const items = Array.from(contacts.getElementsByClassName("contact"));
-  const itemOffsets = items.map((item) => item.offsetTop);
-  const topItemIndex = itemOffsets.findIndex(
-    (offset) => contacts.scrollTop - offset <= -18
-  );
-  if (topItemIndex !== -1) {
-    stickyHeader.textContent = items[topItemIndex].textContent;
-  }
+/*contacts.addEventListener('scroll', (e) => {
+	const items = Array.from(contacts.getElementsByClassName('contact'));
+	const itemOffsets = items.map((item) => item.offsetTop);
+	const topItemIndex = itemOffsets.findIndex(
+		(offset) => contacts.scrollTop - offset <= -18
+	);
+	if (topItemIndex !== -1) {
+		stickyHeader.textContent = items[topItemIndex].textContent;
+	}
+});*/
+
+contacts.addEventListener('scroll', (e) => {
+	const items = Array.from(contacts.getElementsByClassName('contact'));
+	let topContactsList = contacts.scrollTop;
+	const topItemIndex = items.findIndex(
+		(item) => topContactsList - item.offsetTop <= -18
+	);
+	if (topItemIndex !== -1) {
+		stickyHeader.textContent = items[topItemIndex].textContent;
+	}
 });
 
 addContacts();
